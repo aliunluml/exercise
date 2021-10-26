a7f84c6ac6498036f1b7f53853e9e1001f99a2dc
Ali Unlu <33809587+aliunluml@users.noreply.github.com>
2021-10-26 14:40:06 +0300

    prune todo. Last night's progress

--- toy.py
+++ toy.py
@@ -1,31 +1,46 @@
 import re
 import argparse
 
-class Trail:
-    def __init__(self,sum=0,points=[]):
-        self.sum=sum
-        self.points=points
+class Point:
+    def __init__(self,value,pos):
+        self.value=value
+        self.pos=pos
 
-def prune(trails):
+
+class Trail:
+    def __init__(self,points=[]):
+        self.points=points
+        self.sum=sum([p.value for p in points]) if len(points)!=0 else 0
+        self.end=points[-1].pos if len(points)!=0 else (0,0)
+
+    def add_point(self,point):
+        self.points.append(point)
+        self.sum+=point.value
+        self.end=point.pos
+
+def prune(trails,new_trails):
+    len_same_end=len(trails)
 
 
 def main(args):
-    trails=[]
+    start=Trail()
+    trails=[start]
+    y=0
     lower_triangular_matrix_file=open(args.filename,'r')
     for line in lower_triangular_matrix_file:
         row=re.findall('(\d+)',line)
-        # Top of the pyramid
-        if y==0:
-            sum=row[0]
-        # Top-down layers
-        else:
-
-            if row[x]>row[x+1]:
-                sum+=row[x]
-            else:
-                sum+=row[x+1]
-                x+=1
+        row_points=[Point(int(row[i]),(i,y)) for i in range(len(row))]
+        new_trails=[]
+        for trail in trails:
+            pos=trail.end
+            new_trail=trail.copy()
+            trail.add_point(row_points[pos[0]])
+            new_trail.add_point(row_points[pos[0]+1])
+            new_trails.append(new_trail)
+        trails=prune(trails,new_trails)
         y+=1
+
+
 
     txt='The sum is '+str(sum)
     print(txt)
